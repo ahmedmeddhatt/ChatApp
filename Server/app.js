7def99dc8656890cebe7d43dab419e3771457d67
Ahmed Medhat <ahmedmedhat1231@gmail.com>
2023-06-07 14:52:15 +0300

    listining to connection & disconnection from server and client sides

--- Server/app.js
+++ Server/app.js
@@ -1,15 +1,33 @@
 const express = require('express')
-const path = require('path')
-const app = express()
+const http = require('http');
 
+const path = require('path')
 const publicPath = path.join(__dirname, '/../Public')
+
+let app = express()
+
+const server = http.createServer(app);
+const socketIo = require("socket.io");
+const io = socketIo(server);
+
+
 app.use(express.static(publicPath))
 
 app.get('/', function (req, res) {
   res.send('Hello boy')
 })
 
-app.listen(9000, () => {
+io.on('connection', (socket) => {
+  console.log('Connected From Server');
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected From Server');
+  });
+
+});
+
+
+server.listen(9000, () => {
     
     console.log(`Port 9000 is listening ...`);
 })
